ce6380b6e5b26ff1ba88fe123384bf313ad5847e
ThiagoLuizNunes <thiago.luiz@lavid.ufpb.br>
2017-08-26 00:02:57 -0300

    drawLine function was initialized

--- cg_framework/main.cpp
+++ cg_framework/main.cpp
@@ -11,18 +11,29 @@ void MyGlDraw(void)
 	int height, width;
 	int red, green, blue, alpha;
 
-	std::cout << "Type the coordinates height and width at sequences: " << '\n';
-	std::cin >> height >> width;
-	std::cout << "Type the color:" << '\n';
-	std::cin >> red >> green >> blue >> alpha;
 
+	std::vector<int> coordinatesVector;
 	std::vector<int> rgbaVector;
-	rgbaVector.push_back(red);
-	rgbaVector.push_back(green);
-	rgbaVector.push_back(blue);
-	rgbaVector.push_back(alpha);
 
-	putPixel(height, width, rgbaVector);
+
+	// std::cout << "Type the coordinates height and width at sequences: " << '\n';
+	// std::cin >> height >> width;
+	// std::cout << "Type the color:" << '\n';
+	// std::cin >> red >> green >> blue >> alpha;
+
+	coordinatesVector.push_back(50);
+	coordinatesVector.push_back(200);
+	coordinatesVector.push_back(50);
+	coordinatesVector.push_back(150);
+
+
+	rgbaVector.push_back(255);
+	rgbaVector.push_back(255);
+	rgbaVector.push_back(255);
+	rgbaVector.push_back(1);
+
+	// putPixel(height, width, rgbaVector);
+	drawLine(coordinatesVector, rgbaVector);
 
 }
 //-----------------------------------------------------------------------------
--- cg_framework/mygl.h
+++ cg_framework/mygl.h
@@ -8,9 +8,21 @@
 // Defina aqui as suas funções gráficas
 //*****************************************************************************
 
-void printPixel(int x, int y, std::vector<int>& myRGBA) {
+void putPixel(int x, int y, std::vector<int>& myRGBA) {
   for (size_t i = 0; i < 4; i++) {
     FBptr[x*4+i + y*4*IMAGE_WIDTH] = myRGBA.at(i);
+  }
+}
+
+void drawLine(std::vector<int>& coordinates, std::vector<int>& myRGBA) {
+  if(coordinates.at(0) == coordinates.at(2)) {
+    int s = coordinates.at(1) - coordinates.at(3);
+    if(s < 0) {
+      s *= -1;
+    }
+    for (size_t i = 0; i < s; i++) {
+      putPixel(coordinates.at(0), coordinates.at(1) + i, myRGBA);
+    }
   }
 }
 
