a95564abad9723041889ba28ceab582e5fa6f298
ThiagoLuizNunes <thiago.luiz@lavid.ufpb.br>
2017-09-06 23:42:59 -0300

    adding bresenham interpolation

--- cg_framework/main.cpp
+++ cg_framework/main.cpp
@@ -32,7 +32,7 @@ void MyGlDraw(void)
 	coordinatesVector.push_back(0); //x1
 	coordinatesVector.push_back(0); //y1
 	coordinatesVector.push_back(500); //x2
-	coordinatesVector.push_back(500); //y2
+	coordinatesVector.push_back(50); //y2
 
 	rgbaVector.push_back(0);
 	rgbaVector.push_back(0);
@@ -42,13 +42,13 @@ void MyGlDraw(void)
 	// putPixel(width, height, rgbaVector);
 	// drawLine(coordinatesVector, rgbaVector);
 	int j;
-	// for (size_t i = 0; i < 500; i++) {
-	// 	coordinatesVector.at(2) = 500-i;
-	// 	ddaAlgorithm(coordinatesVector, rgbaVector);
-	// 	//bresenhamAlgorithm(coordinatesVector, rgbaVector);
-	// }
+	for (size_t i = 0; i < 500; i++) {
+		coordinatesVector.at(2) = 500-i;
 		// ddaAlgorithm(coordinatesVector, rgbaVector);
 		bresenhamAlgorithm(coordinatesVector, rgbaVector);
+	}
+		// ddaAlgorithm(coordinatesVector, rgbaVector);
+		// bresenhamAlgorithm(coordinatesVector, rgbaVector);
 }
 //-----------------------------------------------------------------------------
 int main(int argc, char **argv)
